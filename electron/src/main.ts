2cbfaccf21049c1e5586bfb4958de68a6b6393cc
Oliver Frost <oliverfrost11@gmail.com>
2024-03-04 21:06:02 +0100

    Deleted window

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -27,6 +27,8 @@ const createWindow = () => {
     frame: true,
   });
 
+  window.setContentProtection(true);
+
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     window.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
